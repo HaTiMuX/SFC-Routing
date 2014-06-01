85dd5e78a042515bef00c2b20e8f220e18e52afb
HaTiMuX <chikhi.hatim@gmail.com>
2014-06-01 12:34:49 -0700

    Neighbor commit

--- DIAG_RES.py
+++ DIAG_RES.py
@@ -3,7 +3,7 @@ from scapy.all import *
 import os, MySQLdb
 
 #Adding iptables rule
-#os.system('iptables -t mangle -A PREROUTING -j NFQUEUE --queue-num 2')
+#os.system('iptables -t mangle -A PREROUTING -j NFQUEUE --queue-num 1')
 os.system('iptables -t mangle -A FORWARD -j NFQUEUE --queue-num 1')
 
 # Open database connection
@@ -19,16 +19,17 @@ class DIAG_RES(Packet):
 
 class DIAG_REQ(Packet):
     name = "DIAG_REQUEST"
-    fields_desc=[ ByteField("Req_ID", 0),
+    fields_desc=[ ByteField("REQ_ID", 0),
                   ShortField("SF_Map_Index", None),
                   FieldLenField("SF_ID_Len", None, length_of="SF_ID"), 
-                  StrLenField("SF_ID", "", length_from=lambda pkt:pkt.SF_ID_Len)]
+                  StrLenField("SF_ID", "", length_from=lambda pkt:pkt.SF_ID_Len),
+                  ByteField("TestPacket", 0)]
 
-
-bind_layers(IP, DIAG_REQ)
-bind_layers(DIAG_REQ, IP)
+bind_layers(IP, DIAG_REQ, proto=253)
+bind_layers(DIAG_REQ, IP, TestPacket=1)
 bind_layers(DIAG_REQ, DIAG_RES)
 bind_layers(DIAG_RES, IP)
+
 
 
 def cb1(payload):
@@ -52,12 +53,22 @@ def cb(payload):
 	data = payload.get_data()
 	p = IP(data)
 	if DIAG_REQ in p:
-		print p[DIAG_REQ].SF_Map_Index
+		test=p[DIAG_REQ]
+		if IP in test:
+			print "Test packet extracted"
+			test = test[IP]
+			test.show()
+		else:
+			print "Error: No Test packet in DIAG_REQ"
+
+
+		print "SF_Map_Index: " + str(p[DIAG_REQ].SF_Map_Index)
 		if p[DIAG_REQ].SF_Map_Index==0: 
 			try:
-				sql = "SELECT SF FROM ServiceFunctions WHERE SF='%s'" % (p[DIAG_REQ].SF_ID)
+				sql = "SELECT SF FROM LocalSFs WHERE SF='%s'" % (p[DIAG_REQ].SF_ID)
 				cursor.execute(sql)
 				result = cursor.fetchone()
+
 				if result is None:
 					print "SF Function not supported"
 					req_id = p[DIAG_REQ].REQ_ID
@@ -69,6 +80,7 @@ def cb(payload):
 					send(res, verbose=0)
 					payload.set_verdict(nfqueue.NF_DROP)
 				else:
+					print "SF Function supported"
 					print "Applying diagnostic on the specified SF Function"
 					#preparing the Diagnostic and sending the response
 					payload.set_verdict(nfqueue.NF_DROP)
@@ -78,12 +90,46 @@ def cb(payload):
 
 		elif p[DIAG_REQ].SF_Map_Index!=1:
 			print "Dignostic of a specific SF Map"
+			try:
+				sql = "SELECT id FROM SFCRoutingTable WHERE SF_MAP_INDEX=%d" % (p[DIAG_REQ].SF_Map_Index)
+				cursor.execute(sql)
+				result = cursor.fetchone()
+				if result is None:
+					print "SF Function not in the specified SF Map"
+					req_id = p[DIAG_REQ].REQ_ID
+					status = 0
+					error = 2
+					dest = p.src 
+					res = IP(dst=dest)/p[DIAG_REQ]/DIAG_RES(REQ_ID = req_id, STATUS = status, ERROR=error)
+					res.show2()
+					send(res, verbose=0)
+					payload.set_verdict(nfqueue.NF_DROP)
+				else:
+					print "Applying diagnostic on the specified SF Function"
+					#preparing the Diagnostic and sending the response
+					
+					sql = "SELECT nextSF FROM SFCRoutingTable WHERE SF_MAP_INDEX=%d" % (p[DIAG_REQ].SF_Map_Index)
+					cursor.execute(sql)
+					result = cursor.fetchone()
+					if result is not None:
+						req_id = p[DIAG_REQ].REQ_ID
+						status = 1
+						error = 0
+						dest = p.src 
+						res = IP(dst=dest)/p[DIAG_REQ]/DIAG_RES(REQ_ID = req_id, STATUS = status, ERROR=error)
+						res.show2()
+						send(res, verbose=0)
+						payload.set_verdict(nfqueue.NF_DROP)
+					payload.set_verdict(nfqueue.NF_DROP)
+			except:
+		   		print "Error: unable to fecth data"
+				payload.set_verdict(nfqueue.NF_DROP)
 			payload.set_verdict(nfqueue.NF_DROP)
 		else: 
-			print "Dignostic of all SF Map"
+			print "Dignostic of all SF Maps"
 			payload.set_verdict(nfqueue.NF_DROP)
 	else:
-		payload.set_verdict(nfqueue.NF_DROP)
+		payload.set_verdict(nfqueue.NF_ACCEPT)
 
 q = nfqueue.queue()
 q.set_callback(cb)
--- Neighbor.py
+++ Neighbor.py
@@ -4,9 +4,6 @@ from scapy.all import *
 import MySQLdb
 import os, sys, time
 
-
-db = MySQLdb.connect("localhost","sfcuser","sfc123","SFC")
-cursor = db.cursor()
 GWs = []
 
 output = check_output(['ip','neighbor'])
@@ -15,7 +12,7 @@ lines = output.split('\n')
 for line in lines:
 	GWs.append(line.split(' ')[0])
 
-print GWs
+print "Local gateways: " +  str(GWs)
 
 i = 1
 while(i <= len(GWs)):
@@ -49,32 +46,42 @@ def Gateway(IP):
 
 while(True):
 	try:
+		db = MySQLdb.connect("localhost","sfcuser","sfc123","SFC")
+		cursor = db.cursor()
+
 		try:
-			sql = "SELECT SF_MAP_INDEX, NextSFHop FROM SFCRoutingTable WHERE Encap IS NULL"
+			sql = "SELECT SF_MAP_INDEX, NextSF FROM SFCRoutingTable WHERE Encap IS NULL"
 		   	cursor.execute(sql)
 			results = cursor.fetchall()
 
 		except:
-			print "Error Reading IP address of Next SF Hop"
+			print "Error Reading next SF in the Map"
 
 		if(results):
 			for result in results:
 				index = result[0]
-				IPx = result[1]
-				if IPx is not None:
+				SF = result[1]
+				if SF is not None:
+					try:
+						sql = "SELECT Locator FROM LocalLocators WHERE SF='%s'" % SF
+					   	cursor.execute(sql)
+						res = cursor.fetchone()
+					except:
+						print "Error Reading IP address of the next SF"
+
+				
+					IPx = res[0]
 					try:
 						pdest = IP(dst=IPx, ttl=1)
 						ans = sr1(pdest, verbose=0)
 						nextHop = ans[IP].src
-						print nextHop
-
 					except:
 						print "Destination unreachable"
 
 					if(IPx==nextHop):
-						sql = "UPDATE SFCRoutingTable SET Encap = 0 WHERE NextSFHop='%s'" % IPx
+						sql = "UPDATE SFCRoutingTable SET Encap = 0 WHERE NextSF='%s'" % SF
 					else:
-						sql = "UPDATE SFCRoutingTable SET Encap = 1 WHERE NextSFHop='%s'" % IPx 
+						sql = "UPDATE SFCRoutingTable SET Encap = 1 WHERE NextSF='%s'" % SF 
 
 					#Force routing in case destination gateway is different from next SF hop gateway
 					GW = Gateway(IPx)
@@ -83,11 +90,13 @@ while(True):
 					else:
 						m = GWs.index(GW) + 1
 					rule = 'iptables -t mangle -A PREROUTING -m tos --tos %d -j MARK --set-mark %d' % (index, m)
-					print rule
+					print "Forcing Rule to add: " + rule + "\n"
 					os.system(rule)
+					print "Updating Encap Done!!"
 
 				else:
 					sql = "UPDATE SFCRoutingTable SET Encap = 0 WHERE SF_MAP_INDEX=%d" % index
+					print "Updating Encap Done!!"
 
 				try:
 				   	cursor.execute(sql)
@@ -98,11 +107,11 @@ while(True):
 
 		else:
 			print "No entry with Encap = NULL"
-	
+
+		db.close()
 		time.sleep(10)
 
 	except KeyboardInterrupt, e:
 		break
 
-db.close()
 
